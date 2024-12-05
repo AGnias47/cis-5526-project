e757c95091bd7364d85cac99b8a5ad4559301718
Andy Gnias <AGnias47@gmail.com>
2024-12-04 21:42:35 -0500

    SVR

--- models/sk_regressors.py
+++ models/sk_regressors.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
 import sys
 
 sys.path.append(".")
@@ -14,6 +15,8 @@ from models.constants import (
     DF_DIRECTORS,
 )
 from models.metrics import mse_V
+
+JOBS = 1
 
 
 def data(df_raw=DF_NO_DIRECTORS):
@@ -32,9 +35,32 @@ def data(df_raw=DF_NO_DIRECTORS):
 def rf_test_depth(X_train, X_test, y_train, y_test, results_fname):
     with open(results_fname, "w") as F:
         F.write("Model,MSE,depth\n")
-    for depth in [2,5,9,10,11,12,13,14,15,16,17,21,25,27,29,32,50,72,100,150,250,None]:
+    for depth in [
+        2,
+        5,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        21,
+        25,
+        27,
+        29,
+        32,
+        50,
+        72,
+        100,
+        150,
+        250,
+        None,
+    ]:
         model = RandomForestRegressor(
-            max_depth=depth, random_state=RANDOM_STATE, verbose=1, n_jobs=-1
+            max_depth=depth, random_state=RANDOM_STATE, verbose=1, n_jobs=JOBS
         )
         model.fit(X_train, y_train)
         Y_hat = model.predict(X_test)
@@ -45,21 +71,43 @@ def rf_test_depth(X_train, X_test, y_train, y_test, results_fname):
         print(f"RandomForestRegressor,{MSE},{depth}")
 
 
-def rf_no_directors_model(results_fname, df=DF_NO_DIRECTORS, depth=15):
+def rf(results_fname, df=DF_NO_DIRECTORS, depth=15):
     X_train, X_test, y_train, y_test = data(df)
     model = RandomForestRegressor(
-        max_depth=depth, random_state=RANDOM_STATE, verbose=1, n_jobs=-1
+        max_depth=depth, random_state=RANDOM_STATE, verbose=1, n_jobs=JOBS
     )
+    MSE, r2 = run_model(model, X_train, X_test, y_train, y_test)
+    print(f"MSE: {round(MSE, 2)}, R2: {round(r2, 2)}")
+    with open(results_fname, "a") as F:
+        F.write(f"Model,MSE,R2,depth\n")
+        F.write(f"RandomForestRegressor,{MSE},{r2},{depth}\n")
+    return model
+
+
+def svr(results_fname, df=DF_NO_DIRECTORS):
+    X_train, X_test, y_train, y_test = data(df)
+    C = 1.0
+    epsilon = 0.1
+    model = SVR(
+        kernel="rbf", degree=3, C=1.0, epsilon=0.1, verbose=True, cache_size=4096
+    )
+    MSE, r2 = run_model(model, X_train, X_test, y_train, y_test)
+    print(f"MSE: {round(MSE, 2)}, R2: {round(r2, 2)}")
+    with open(results_fname, "a") as F:
+        F.write(f"Model,MSE,R2,C,Epsilon\n")
+        F.write(f"SVR,{MSE},{r2},{C},{epsilon}\n")
+    return model
+
+
+def run_model(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     Y_hat = model.predict(X_test)
     E = y_test - Y_hat
     MSE = mse_V(E)
     r2 = model.score(X_test, y_test)
-    print(f"MSE: {round(MSE, 2)}, R2: {round(r2, 2)}")
-    with open(results_fname, "a") as F:
-        F.write(f"RandomForestRegressor,{MSE},{depth}\n")
-    return model
+    return MSE, r2
 
 
 if __name__ == "__main__":
-    rf_no_directors_model(results_fname="results/directors/rf.csv", df=DF_DIRECTORS)
+    rf(results_fname="results/no_dir/rf.csv")
+    svr(results_fname="results/no_dir/svr.csv")
